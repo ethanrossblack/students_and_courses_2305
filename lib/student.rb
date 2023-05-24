353499281cf247f682acbfd7f7bbb1f65426d466
Ethan Black <ethanrossblack@gmail.com>
2023-05-24 09:11:42 -0600

    feat: initialize and test Student class

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,8 @@
 class Student
- 
+  attr_reader :name, :age
+
+  def initialize(info_hash)
+    @name = info_hash[:name]
+    @age = info_hash[:age]
+  end
 end
--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -2,5 +2,17 @@ require 'rspec'
 require './lib/student'
 
 RSpec.describe Student do
-  
+  before do
+    @student = Student.new({name: "Morgan", age: 21})
+  end
+
+  describe "#initialize" do
+    it "exists and has readable attributes" do
+      expect(@student).to be_a Student
+      expect(@student.name).to eq "Morgan"
+      expect(@student.age).to eq 21
+    end
+  end
+
+
 end
