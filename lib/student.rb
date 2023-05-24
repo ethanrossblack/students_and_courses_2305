54d166a5ec57bcdb6c6496091e721c72ab57f7ae
Ethan Black <ethanrossblack@gmail.com>
2023-05-24 09:15:09 -0600

    feat: add log_score method and test to Student

--- lib/student.rb
+++ lib/student.rb
@@ -1,8 +1,13 @@
 class Student
-  attr_reader :name, :age
+  attr_reader :name, :age, :scores
 
   def initialize(info_hash)
     @name = info_hash[:name]
     @age = info_hash[:age]
+    @scores = []
+  end
+
+  def log_score(score)
+    @scores << score
   end
 end
--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -11,6 +11,16 @@ RSpec.describe Student do
       expect(@student).to be_a Student
       expect(@student.name).to eq "Morgan"
       expect(@student.age).to eq 21
+      expect(@student.scores).to eq []
+    end
+  end
+
+  describe "#log_score" do
+    it "can log scores" do
+      @student.log_score(89)
+      @student.log_score(78)
+      
+      expect(@student.scores).to eq [89, 78]
     end
   end
 
