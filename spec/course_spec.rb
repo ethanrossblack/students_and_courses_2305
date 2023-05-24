6b99493df51c81ad73b5c4639a643a01173a98e7
Ethan Black <ethanrossblack@gmail.com>
2023-05-24 09:31:21 -0600

    feat: initialize and test Course class

--- spec/course_spec.rb
+++ spec/course_spec.rb
@@ -3,5 +3,21 @@ require './lib/course'
 require './lib/student'
 
 RSpec.describe Course do 
+  before do
+    @course = Course.new("Calculus", 2)
 
+    @morgan = Student.new({name: "Morgan", age: 21})
+    @jordan = Student.new({name: "Jordan", age: 29})
+  end
+
+  define "#initialize" do
+    it "exists and has readable attributes" do
+      expect(@course).to be_a Course
+      expect(@course.name).to eq "Calculus"
+      expect(@course.capacity).to eq 2
+      expect(@course.students).to eq []
+    end
+  end
+
+  
 end
